4e6ad6f9d06ca387018c6e44a5d5d41104877a75
Malikalugun <105351482+Malikalugun@users.noreply.github.com>
2024-02-13 22:47:26 +0530

    dynamic data by array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 // image dynamic
-import reactImage from './assets/react-core-concepts.png'
+import reactImage from './assets/react-core-concepts.png';
+import ComponentImg from './assets/components.png';
+ import {CORE_CONCEPTS} from './data.js';
+
 // text dynamic
 const reactDescriptions = ['Fundamental','Crucial','Core'];
 function genRandomInt(max){
@@ -20,12 +23,40 @@ function Header(){
       </header>
   );
 }
-
+// function CoreConcept(props){
+  function CoreConcept({image,title,description}){
+  
+  return <li>
+    {/* <img src={props.image} alt=".." />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p> */}
+    <img src={image} alt=".." />
+    <h3>{title}</h3>
+    <p>{description}</p> 
+  </li>
+}
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <section id="core-concepts">
+<h2>Core Concepts</h2>
+<ul>
+  {/* <CoreConcept title="Components" description="The core Ui building bolck." image={ComponentImg}/>
+  <CoreConcept title="props"/>
+  <CoreConcept />
+  <CoreConcept /> */}
+  {/* <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+  <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+  <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+  <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/> */}
+   <CoreConcept {...CORE_CONCEPTS[0]}/>
+   <CoreConcept {...CORE_CONCEPTS[1]}/>
+   <CoreConcept {...CORE_CONCEPTS[2]}/>
+   <CoreConcept {...CORE_CONCEPTS[3]}/>
+</ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import ComponentImg from './assets/components.png';
+import propsImg from './assets/config.png';
+import jsxImg from './assets/jsx-ui.png';
+import stateImg from './assets/state-mgmt.png';
+export const CORE_CONCEPTS = [
+ {
+  image : ComponentImg,
+  title : 'Component',
+  description : 'The components Ui building bolck',
+ },
+ {
+  image : propsImg,
+  title : 'Propos',
+  description : 'The props Ui building bolck',
+ },
+ {
+  image : jsxImg,
+  title : 'jsx',
+  description : 'The jsx Ui building bolck',
+ },
+ {
+  image : stateImg,
+  title : 'state',
+  description : 'The state Ui building bolck',
+ },
+ 
+]
